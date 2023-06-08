93c4cf83e7735b88e0753e9ec058f96fedfa7ef8
jeeramethgfs <jeerameth.k@genfosis.com>
2023-06-08 15:51:57 +0700

    fix header-body-footer to css naja

--- src/components/dcv-health-report/cards/RecommandCardComponet.vue
+++ src/components/dcv-health-report/cards/RecommandCardComponet.vue
@@ -1,35 +1,27 @@
 <template>
     <div class="flex flex-row flex-wrap pl-11 pt-[11.5rem]">
         <p class="font-notoSansThai font-bold text-black text-small_long pl-8 pt-1">{{ groupName }}</p>
-        <div class="grid grid-rows-[auto,1fr,auto] w-[44rem] h-28 pt-[0.3rem] pl-2">
-            <header class="background-header bg-cover bg-no-repeat w-[44.4rem] h-[0.4rem]"></header>
-            <main class="background-body bg-no-repeat bg-cover flex flex-col w-full">
-                <div v-for="(item, index) in groupList">
-                    <div class="background-body bg-no-repeat bg-cover flex flex-row pt-2">
-                        <div class="pl-5 flex-initial w-64">
-                            <p class="font-notoSansThai text-very_small" v-html="item.name"></p>
+        <div class="grid grid-rows-[auto,1fr,auto] w-[44rem] h-28 pt-[0.3rem] ">
+            <!-- start here -->
+            <div class="relative drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)] ring-1 ring-gray-900/5 mx-auto rounded-lg w-[684px] bg-white">
+                <div class="bg-red-600 w-full h-auto p-1 rounded-t-lg"></div>
+                <div class="relative py-5 px-2 shadow-xl ring-1 ring-gray-900/5 mx-auto  rounded-lg">
+                    <div v-for="(item, index) in groupList">
+                        <div class="grid grid-cols-10 w-auto ">
+                            <p class="font-notoSansThai text-[10px] text-gray-600 text-justify  col-span-2 px-4 font-bold" v-html="item.name"></p>
+                            <p class="font-notoSansThai text-[10px] text-gray-600 text-right  col-span-1 px-4">{{ item.score }}</p>
+                            <p class="font-notoSansThai text-[10px] text-gray-600 text-justify  col-span-3 px-4">{{ item.checkup }}</p>
+                            <div class="font-notoSansThai text-[10px] text-gray-600 text-justify col-span-4 px-4 flex-col">
+                                <p class="">{{ item.risk_reduction }}</p>
+                                <p class="text-gray-600 font-bold">อาหารเสริมที่แนะนำ: </p>
+                                <!-- add new item -->
+                                <p class="">{{ item.supplement }}</p>
+                            </div>
                         </div>
-                        <div class="pl-20 flex-initial w-[18rem] ">
-                            <p class="font-notoSansThai text-very_small">
-                                {{ item.score }}
-                            </p>
-                        </div>
-                        <div class="">
-                            <p class="font-notoSansThai text-very_small">
-                                {{ item.checkup }}
-                            </p>
-                        </div>
-                        <div class="pl-14 pr-9">
-                            <p class="font-notoSansThai text-very_small">
-                                <!-- teat -->
-                                {{ item.risk_reduction }}
-                            </p>
-                        </div>
+                        <hr class="border-t-1 ml-5 my-3 border-gray-300 w-[40rem] " v-if="isNotLastIndex(index)">
                     </div>
-                    <hr class="border-t-1 ml-5 mt-5 border-gray-300 w-[40rem] pb-2" v-if="isNotLastIndex(index)">
                 </div>
-            </main>
-            <footer class="background-footer bg-cover bg-no-repeat w-full h-5"></footer>
+            </div>
         </div>
     </div>
 </template>
@@ -45,19 +37,22 @@ const props = defineProps({
                 name: 'โรคหลอดเลือดสมอง <br> (Stroke)',
                 score: '10.00',
                 checkup: 'ตรวจเช็คร่างกาย โดยเฉพาะ ความดันโลหิต ระดับน้ำตาล และไขมันในเลือด เนื่องจากเป็นปัจจัยสำคัญในการเกิดโรค',
-                risk_reduction: 'ออกกำลังกายสม่ำเสมอ เน้นระดับความเข้มข้นปานกลาง (zone 2-3) ระยะเวลาไม่ต่ำกว่า 30 นาที อย่างน้อย 150 นาทีต่อสัปดาห์ เพื่อส่งเสริมการทำงานของหัวใจ และทำให้ร่างกายใช้พลังงานไขมันและน้ำตาลได้ดี '
+                risk_reduction: 'ออกกำลังกายสม่ำเสมอ เน้นระดับความเข้มข้นปานกลาง (zone 2-3) ระยะเวลาไม่ต่ำกว่า 30 นาที อย่างน้อย 150 นาทีต่อสัปดาห์ เพื่อส่งเสริมการทำงานของหัวใจ และทำให้ร่างกายใช้พลังงานไขมันและน้ำตาลได้ดี ',
+                supplement: 'กินแกลบจ้า'
             },
             {
                 name: 'โรคหลอดเลือดสมอง <br> (Stroke)',
                 score: '10.00',
                 checkup: 'ตรวจเช็คร่างกาย โดยเฉพาะ ความดันโลหิต ระดับน้ำตาล และไขมันในเลือด เนื่องจากเป็นปัจจัยสำคัญในการเกิดโรค',
-                risk_reduction: 'ออกกำลังกายสม่ำเสมอ เน้นระดับความเข้มข้นปานกลาง (zone 2-3) ระยะเวลาไม่ต่ำกว่า 30 นาที อย่างน้อย 150 นาทีต่อสัปดาห์ เพื่อส่งเสริมการทำงานของหัวใจ และทำให้ร่างกายใช้พลังงานไขมันและน้ำตาลได้ดี '
+                risk_reduction: 'ออกกำลังกายสม่ำเสมอ เน้นระดับความเข้มข้นปานกลาง (zone 2-3) ระยะเวลาไม่ต่ำกว่า 30 นาที อย่างน้อย 150 นาทีต่อสัปดาห์ เพื่อส่งเสริมการทำงานของหัวใจ และทำให้ร่างกายใช้พลังงานไขมันและน้ำตาลได้ดี ',
+                supplement: 'กินแกลบจ้า'
             },
             {
                 name: 'โรคหลอดเลือดสมอง <br> (Stroke)',
                 score: '10.00',
                 checkup: 'ตรวจเช็คร่างกาย โดยเฉพาะ ความดันโลหิต ระดับน้ำตาล และไขมันในเลือด เนื่องจากเป็นปัจจัยสำคัญในการเกิดโรค',
-                risk_reduction: 'ออกกำลังกายสม่ำเสมอ เน้นระดับความเข้มข้นปานกลาง (zone 2-3) ระยะเวลาไม่ต่ำกว่า 30 นาที อย่างน้อย 150 นาทีต่อสัปดาห์ เพื่อส่งเสริมการทำงานของหัวใจ และทำให้ร่างกายใช้พลังงานไขมันและน้ำตาลได้ดี '
+                risk_reduction: 'ออกกำลังกายสม่ำเสมอ เน้นระดับความเข้มข้นปานกลาง (zone 2-3) ระยะเวลาไม่ต่ำกว่า 30 นาที อย่างน้อย 150 นาทีต่อสัปดาห์ เพื่อส่งเสริมการทำงานของหัวใจ และทำให้ร่างกายใช้พลังงานไขมันและน้ำตาลได้ดี ',
+                supplement: 'กินแกลบจ้า'
             },
         ]
     },
@@ -69,15 +64,5 @@ function isNotLastIndex(index: number): boolean {
 </script>
 
 <style scoped>
-.background-header {
-    background-image: v-bind(headerState);
-}
 
-.background-body {
-    background-image: url('/src/assets/img/dcv-report/large-box/large-box-body.png');
-}
-
-.background-footer {
-    background-image: url('/src/assets/img/dcv-report/large-box/large-box-footer.png');
-}
 </style>
