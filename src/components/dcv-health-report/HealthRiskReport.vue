6b1ad392e6ccf2ed4dfcb10496129075fdaec421
Suriya Kenma <bekzerker@gmail.com>
2023-06-09 15:40:31 +0700

    update - bind data with page 1 left section

--- src/components/dcv-health-report/HealthRiskReport.vue
+++ src/components/dcv-health-report/HealthRiskReport.vue
@@ -7,22 +7,55 @@
             <div class="flex flex-row flex-wrap pl-12 py-[9.6rem]">
                 <div class="basis-1/2 pl-1 pt-[1.8rem]">
                     <div class="grid gap-[0.70rem]">
-                        <SmallCardComponent :background-state="`url(${smallCardState.low})`" :line-state="lineState.low" :line-no="1"/>
-                        <SmallCardComponent :background-state="`url(${smallCardState.low})`" :line-state="lineState.low" :line-no="2"/>
-                        <SmallCardComponent :background-state="`url(${smallCardState.low})`" :line-state="lineState.low" :line-no="3"/>
-                        <SmallCardComponent :background-state="`url(${smallCardState.low})`" :line-state="lineState.low" :line-no="4"/>
-                        <SmallCardComponent :background-state="`url(${smallCardState.low})`" :line-state="lineState.low" :line-no="5"/>
+                        <SmallCardComponent 
+                            :background-state="`url(${setSmallCardColor('brain')})`" 
+                            :line-state="setLineColor('brain')"
+                            :name="props.groupScore['brain'][0].name" :score="props.groupScore['brain'][0].score"
+                            :line-no="1" />
+                        <SmallCardComponent 
+                            :background-state="`url(${setSmallCardColor('eent')})`" 
+                            :line-state="setLineColor('eent')"
+                            :name="props.groupScore['eent'][0].name" :score="props.groupScore['eent'][0].score"
+                            :line-no="2" />
+                        <SmallCardComponent 
+                            :background-state="`url(${setSmallCardColor('cardiovascular')})`" 
+                            :line-state="setLineColor('cardiovascular')"
+                            :name="props.groupScore['cardiovascular'][0].name"
+                            :score="props.groupScore['cardiovascular'][0].score" :line-no="3" />
+                        <SmallCardComponent 
+                            :background-state="`url(${setSmallCardColor('chest_abdo')})`" 
+                            :line-state="setLineColor('chest_abdo')"
+                            :name="props.groupScore['chest_abdo'][0].name" 
+                            :score="props.groupScore['chest_abdo'][0].score"
+                            :line-no="4" />
+                        <SmallCardComponent 
+                            :background-state="`url(${setSmallCardColor('infectious')})`" 
+                            :line-state="setLineColor('infectious')"
+                            :name="props.groupScore['infectious'][0].name" 
+                            :score="props.groupScore['infectious'][0].score"
+                            :line-no="5" />
                         <div class="pt-2 pb-2">
-                            <SmallCardComponent :addition-size="true" :background-state="`url(${smallCardState.low})`" :line-state="lineState.low" :line-no="6"/>
+                            <SmallCardComponent 
+                                :addition-size="true" 
+                                :background-state="`url(${setSmallCardColor('urogenital')})`"
+                                :line-state="setLineColor('urogenital')"
+                                :name="props.groupScore['urogenital'][0].name"
+                                :score="props.groupScore['urogenital'][0].score" 
+                                :line-no="6" />
                         </div>
                         <div class="pt-[5rem]">
-                            <SmallCardComponent :background-state="`url(${smallCardState.low})`" :line-state="lineState.low" :line-no="7"/>
+                            <SmallCardComponent 
+                                :background-state="`url(${setSmallCardColor('joint')})`" 
+                                :line-state="setLineColor('joint')"
+                                :name="props.groupScore['joint'][0].name" 
+                                :score="props.groupScore['joint'][0].score"
+                                :line-no="7" />
                         </div>
                     </div>
                 </div>
                 <div class="grid basis-1/2 gap-4 pl-1">
                     <div v-for="item in sampleDataHighestScore">
-                        <CardComponent class="bg-cover w-[20.4rem] h-64" :background-state="`url(${cardState.low})`"
+                        <CardComponent class="bg-cover w-[20.4rem] h-64"
                             :name="item.risk_disease" :score="item.disease_score" :intro="item.intro" />
                     </div>
                 </div>
@@ -36,7 +69,8 @@ import CardComponent from './cards/CardComponent.vue';
 import SmallCardComponent from './cards/SmallCardComponent.vue';
 const props = defineProps({
     background: String,
-    sampleDataHighestScore: { type: Array<DcvHealth>, required: true }
+    sampleDataHighestScore: { type: Array<DcvHealth>, required: true },
+    groupScore: <any>Object
 });
 // console.log(props.sampleDataHighestScore)
 const cardState = {
@@ -54,5 +88,22 @@ const lineState = {
     high: '/src/assets/img/dcv-report/line/lineX_high.png',
     med: '/src/assets/img/dcv-report/line/lineX_med.png',
     low: '/src/assets/img/dcv-report/line/lineX_low.png'
+}
+
+function setLineColor(name: any): string {
+    const score = props.groupScore[name][0].score
+    let state: string = '';
+    if (score >= 0 && score <= 3) return state = lineState.low
+    if (score > 3 && score <= 6) return state = lineState.med
+    if (score > 6 && score <= 10) return state = lineState.high
+    return state;
+}
+function setSmallCardColor(name: any): string {
+    const score = props.groupScore[name][0].score
+    let state: string = '';
+    if (score >= 0 && score <= 3) return state = smallCardState.low
+    if (score > 3 && score <= 6) return state = smallCardState.med
+    if (score > 6 && score <= 10) return state = smallCardState.high
+    return state;
 }
 </script>
--- src/components/dcv-health-report/cards/SmallCardComponent.vue
+++ src/components/dcv-health-report/cards/SmallCardComponent.vue
@@ -3,24 +3,25 @@
         <div class="bg w-[7rem] h-12 bg-contain bg-no-repeat">
             <div class="flex flex-row gap-4 align-middle">
                 <p class="pl-2 pt-2 font-notoSansThai font-bold text-white text-very_small">{{ name }}</p>
-                <p class="pt-2 font-notoSansThai font-bold text-white text-very_small">{{ score }}</p>
+                <p class="pt-2 font-notoSansThai font-bold text-white text-very_small">{{ setScoreString(score) }}</p>
             </div>
         </div>
-        <div class="absolute pl-[7.3rem] mt-[-2.2rem]" >
+        <div class="absolute pl-[7.3rem] mt-[-2.2rem]">
             <!-- แก้ warning ยังไงอ่ะ 555555 โง่จ๊าด เขียน TS ไม่เป็นคับผม -->
-            <img class="h-full" :class="lineClassState[props.lineNo]" :src="props.lineState.replace(/X/,props.lineNo)" >
+            <img class="h-full" :class="lineClassState[props.lineNo]" :src="props.lineState.replace(/X/, props.lineNo.toString())">
         </div>
     </div>
     <div v-else>
         <div class="bg w-[8.4rem] h-14 bg-contain bg-no-repeat">
             <div class="flex flex-row gap-4 align-middle">
                 <p class="pl-2 pt-1 font-notoSansThai font-bold text-white text-very_small" v-html="nameLong"></p>
-                <p class="font-notoSansThai font-bold text-white text-very_small self-center">{{ score }}</p>
+                <p class="font-notoSansThai font-bold text-white text-very_small self-center">{{ setScoreString(score) }}
+                </p>
             </div>
         </div>
-        <div class="absolute pl-[8.5rem] mt-[-2.7rem]" >
+        <div class="absolute pl-[8.5rem] mt-[-2.7rem]">
             <!-- แก้ warning ยังไงอ่ะ 555555 โง่จ๊าด เขียน TS ไม่เป็นคับผม -->
-            <img class="h-full" :class="lineClassState[props.lineNo]" :src="props.lineState.replace(/X/,props.lineNo)" >
+            <img class="h-full" :class="lineClassState[props.lineNo]" :src="props.lineState.replace(/X/, props.lineNo.toString())">
         </div>
     </div>
 </template>
@@ -30,14 +31,14 @@
 const props = defineProps({
     backgroundState: { type: String, required: true },
     lineState: { type: String, required: true },
-    lineNo: { type: String, required: true },
+    lineNo: { type: Number, required: true },
     additionSize: { type: Boolean, required: false, default: false },
     name: { type: String, required: true, default: 'อกและท้อง' },
     nameLong: { type: String, required: false, default: 'ทางเดินปัสสาวะ<br>และสืบพันธุ์' },
-    score: { type: String, required: true, default: '10.00' }
+    score: { type: Number, required: true, default: 10.00 }
 })
 // const lineStateNo = props.lineState.replace(/X/,props.lineNo)
-const lineClassState = {
+const lineClassState: any = {
     1: 'w-[8.5rem]',
     2: 'w-[8.5rem]',
     3: 'w-[1.8rem]',
@@ -45,6 +46,11 @@ const lineClassState = {
     5: 'w-[8.5rem]',
     6: 'w-[8rem]',
     7: 'w-[8.5rem] -mt-36'
+}
+
+
+function setScoreString(score: number): string {
+    return score.toFixed(2).toString();
 }
 </script>
 
