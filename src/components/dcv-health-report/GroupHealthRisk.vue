3d2058bc3bcffa75bf27c39eae5594d5bd493ee4
jeeramethgfs <jeerameth.k@genfosis.com>
2023-06-09 15:49:48 +0700

    fix 2nd page

--- src/components/dcv-health-report/GroupHealthRisk.vue
+++ src/components/dcv-health-report/GroupHealthRisk.vue
@@ -3,21 +3,43 @@
         <div :class="['w-full h-full bg-no-repeat bg-cover m-0', props.background]">
             <div class="flex flex-row flex-wrap pl-12 pt-28">
                 <div class="basis-1/2 pt-3">
-                    <CardComponent class="w-20 pb-4" :padding-bottom="'0.3rem'" :background-state="`url(${cardState.low})`"
-                        :group-list="true" />
-                    <CardComponent class="w-20 pb-4" :padding-bottom="'0.01rem'" :background-state="`url(${cardState.low})`"
-                        :group-list="true" />
-                    <CardComponent class="w-20 pb-4" :background-state="`url(${cardState.low})`" :group-list="true" />
-                    <CardComponent class="w-20" :padding-bottom="'5rem'" :background-state="`url(${cardState.low})`"
-                        :group-list="true" />
+                    <CardComponent class="w-20 pb-4" :padding-bottom="'0.3rem'" 
+                        :group-list="true" 
+                        :name="diseaseName[1]" 
+                        :score="diseaseScore[1]"
+                        :group-list-score="getGroupScore('brain')" />
+                    <CardComponent class="w-20 pb-4" :padding-bottom="'0.01rem'" 
+                        :group-list="true" 
+                        :name="diseaseName[2]" 
+                        :score="diseaseScore[2]"
+                        :group-list-score="getGroupScore('cardiovascular')" />
+                    <CardComponent class="w-20 pb-4"  
+                        :group-list="true"
+                        :name="diseaseName[3]" 
+                        :score="diseaseScore[3]" 
+                        :group-list-score="getGroupScore('chest_abdo')" />
+                    <CardComponent class="w-20" :padding-bottom="'5rem'" 
+                        :group-list="true" 
+                        :name="diseaseName[4]" 
+                        :score="diseaseScore[4]"
+                        :group-list-score="getGroupScore('eent')" />
                 </div>
-                <div class="basis-1/2 pl-1">
-                    <CardComponent class="w-20 pb-4" :padding-bottom="'15.5rem'" :background-state="`url(${cardState.low})`"
-                        :group-list="true" />
-                    <CardComponent class="w-20 pb-4" :padding-bottom="'0.3rem'" :background-state="`url(${cardState.low})`"
-                        :group-list="true" />
-                    <CardComponent class="w-20 pb-4" :padding-bottom="'0.3rem'" :background-state="`url(${cardState.low})`"
-                        :group-list="true" />
+                <div class="basis-1/2 pl-1 pt-3">
+                    <CardComponent class="w-20 pb-4" :padding-bottom="'15.5rem'" 
+                        :group-list="true" 
+                        :name="diseaseName[5]" 
+                        :score="diseaseScore[5]"
+                        :group-list-score="getGroupScore('infectious')" />
+                    <CardComponent class="w-20 pb-4" :padding-bottom="'0.3rem'" 
+                        :group-list="true"
+                        :name="diseaseName[6]" 
+                        :score="diseaseScore[6]"
+                        :group-list-score="getGroupScore('joint')" />
+                    <CardComponent class="w-20 pb-4" :padding-bottom="'0.3rem'" 
+                        :group-list="true"
+                        :name="diseaseName[7]" 
+                        :score="diseaseScore[7]"
+                        :group-list-score="getGroupScore('urogenital')" />
                 </div>
             </div>
         </div>
@@ -25,14 +47,80 @@
 </template>
 
 <script lang="ts" setup>
+import { onUpdated, ref } from 'vue';
 import CardComponent from './cards/CardComponent.vue';
 const props = defineProps({
-    background: String
+    background: { type: String },
+    groupSample: { type: Object, default: '' }
 })
-const cardState = {
-    high: '/src/assets/img/dcv-report/large-box/large-box-high.png',
-    med: '/src/assets/img/dcv-report/large-box/large-box-med.png',
-    low: '/src/assets/img/dcv-report/large-box/large-box-low.png',
 
+const diseaseName = ref(['EMPTY']);
+const diseaseScore = ref([0.00]);
+
+onUpdated(() => {
+})
+
+const groupName = ref([
+    {
+        group: 'brain',
+        name: 'กลุ่มโรคสมอง',
+    },
+    {
+        group: 'cardiovascular',
+        name: 'กลุ่มโรคหัวใจและหลอดเลือด',
+    },
+    {
+        group: 'chest_abdo',
+        name: 'กลุ่มโรคปอดและท้อง',
+    },
+    {
+        group: 'eent',
+        name: 'กลุ่มโรคหูตาคอจมูก',
+    },
+    {
+        group: 'infectious',
+        name: 'กลุ่มโรคติดเชื้อ',
+    },
+    {
+        group: 'joint',
+        name: 'กลุ่มโรคข้อต่อ',
+    },
+    {
+        group: 'urogenital',
+        name: 'กลุ่มโรคทางเดินปัสสาวะและสืบพันธ์ุ',
+    },
+])
+
+function getGroupScore(name: string): any[] {
+    let sampleGroup: any[] = [];
+    let found = false; // Flag variable to track the exact name match
+
+    groupName.value.forEach((element: any) => {
+        if (element === undefined) return;
+        if (name === element.group) {
+            const group = props.groupSample[name];
+            diseaseName.value.push(element.name);
+            let sum = 0;
+
+            group.forEach((sample: any) => {
+                sum += sample.disease_score;
+                sampleGroup.push({
+                    name: sample.risk_disease,
+                    score: sample.disease_score,
+                });
+            });
+
+            diseaseScore.value.push(sum / group.length);
+            found = true; // Set the flag to true when the exact name match is found
+        }
+
+        if (found) {
+            return; // Exit the loop if the exact name match has been found
+        }
+    });
+
+    console.log(sampleGroup);
+    return sampleGroup;
 }
+
 </script>
--- src/components/dcv-health-report/cards/CardComponent.vue
+++ src/components/dcv-health-report/cards/CardComponent.vue
@@ -1,25 +1,25 @@
 <template>
     <div v-if="!groupList">
-        <div class="relative drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)] 
+        <div class="relative drop-shadow-[0_4px_4px_rgba(0,0,0,0.2)] 
                 ring-1 ring-gray-900/5 mx-auto max-w-lg rounded-lg w-80 bg-white">
             <div :class="setColor()" class="w-full h-auto p-1 rounded-t-lg"></div>
             <div class="relative pb-5 px-2 shadow-xl ring-1 ring-gray-900/5 mx-auto max-w-lg rounded-lg">
                 <!-- replace below with v-if -->
                 <div class="mx-auto max-w-md">
                     <div class="flex flex-row justify-center gap-12 pt-4">
-                        <p class="font-notoSansThai text-gray-800 font-bold " v-html="name"></p>
+                        <p class="font-notoSansThai text-gray-800 font-bold ml-5" v-html="name"></p>
                         <div class="relative ">
                             <div :class="setColor()"
                                 class="flex w-full my-auto items-center justify-center py-1.5 px-5 rounded-lg">
                                 <!-- score here -->
-                                <p class="font-notoSansThai text-white">{{ score }}</p>
+                                <p class="font-notoSansThai text-white">{{ setScoreString(score) }}</p>
                             </div>
                         </div>
                     </div>
                     <hr class="border-t-1 px-7 mt-5 border-gray-300 w-auto ">
-                    <div class="w-auto  pl-5 pr-2 pt-6 pb-10">
+                    <div class="w-auto pl-5 pr-5 pt-6 pb-7">
                         <!-- intro here -->
-                        <p class="font-notoSansThai text-[13px] text-gray-600 text-justify  w-full">
+                        <p class="font-notoSansThai text-[13px] text-gray-600 w-full">
                             {{ intro }}
                         </p>
                     </div>
@@ -41,14 +41,14 @@
             </div> -->
     </div>
     <div v-else>
-        <div class="relative drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)] 
+        <div class="relative drop-shadow-[0_4px_4px_rgba(0,0,0,0.2)] 
                 ring-1 ring-gray-900/5 mx-auto max-w-lg rounded-lg w-80 bg-white">
             <div :class="setColor()" class="w-full h-auto p-1 rounded-t-lg"></div>
             <div class="relative pb-5 px-2 shadow-xl ring-1 ring-gray-900/5 mx-auto max-w-lg rounded-lg">
                 <!-- replace below with v-if -->
                 <div class="mx-auto max-w-md">
-                    <div class="flex flex-row justify-center gap-12 pt-4">
-                        <p class="font-notoSansThai text-gray-800 font-bold " v-html="name"></p>
+                    <div class="flex flex-row justify-center gap-12 pt-4 pl-5 pr-2">
+                        <p class="font-notoSansThai text-gray-800 font-bold mr-auto" v-html="name"></p>
                         <div class="relative ">
                             <div :class="setColor()" class="flex w-full my-auto items-center justify-center py-1.5 px-5 rounded-lg">
                                 <!-- score here -->
@@ -56,12 +56,12 @@
                             </div>
                         </div>
                     </div>
-                    <hr class="border-t-1 px-7 mt-5 border-gray-300 w-auto ">
-                    <div class="w-auto  pl-5 pr-2 pt-6 padding-b">
+                    <hr class="border-t-1 px-7 mt-3 border-gray-300 w-11/12 mx-auto">
+                    <div class="w-auto pl-5 pr-2 pt-5 pb-2">
                         <!-- intro here -->
                         <div v-for="item in groupListScore" class="flex flex-row gap-24">
-                            <p class="font-notoSansThai text-small_long text-gray-600 text-justify"> {{ item.name }}</p>
-                            <p class="font-notoSansThai text-small_long text-gray-600 text-justify">
+                            <p class="font-notoSansThai text-very_small text-gray-600 text-left mr-auto"> {{ item.name }}</p>
+                            <p class="font-notoSansThai text-very_small text-gray-600 ml-auto mt-0.5">
                                 {{ setScoreString(item.score) }}
                             </p>
                         </div>
@@ -73,12 +73,11 @@
 </template>
 <script setup lang="ts">
 const props = defineProps({
-    backgroundState: { type: String, required: true },
-    paddingBottom: { type: String, default: '0.5rem' },
     name: { type: String, required: true, default: 'มะเร็งลำไส้ <br> (Colorectal cancer)' },
     score: { type: Number, required: true, default: 1.1 },
     intro: { type: String, required: true, default: 'มะเร็งลำไส้ใหญ่และทวารหนักเป็นมะเร็งพบบ่อยเป็นอันดับที่3ของคนไทยอาการมีได้ตั้งแต่การขับถ่ายผิดปกติถ่ายอุจจาระเป็นมูกเลือดหรือเป็นเลือดแดงสดจนถึงซีดเบื่ออาหารและน้ำหนักลดโดยไม่ทราบสาเหตุ' },
-    groupList: { type: Boolean, required: true, default: false },
+    groupList: { type: Boolean, required: false, default: false },
+    paddingBottom: { type: String, default: '0.5rem' },
     extraStretch: { type: Boolean, required: false, default: false },
     groupListScore: {
         type: Array<{ name: string, score: number }>, default: [
@@ -117,10 +116,6 @@ function setColor(): string {
 </script>
 
 <style scoped>
-.bg {
-    background-image: v-bind("props.backgroundState")
-}
-
 .padding-b {
     padding-bottom: v-bind("props.paddingBottom");
 }
