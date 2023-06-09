c7a393fd8200f3b7a4863d045cc30760fb6134b0
jeeramethgfs <jeerameth.k@genfosis.com>
2023-06-09 13:19:47 +0700

    line update

--- src/components/dcv-health-report/HealthRiskReport.vue
+++ src/components/dcv-health-report/HealthRiskReport.vue
@@ -7,23 +7,24 @@
             <div class="flex flex-row flex-wrap pl-12 py-[9.6rem]">
                 <div class="basis-1/2 pl-1 pt-[1.8rem]">
                     <div class="grid gap-[0.70rem]">
-                        <SmallCardComponent :background-state="`url(${smallCardState.low})`" />
-                        <SmallCardComponent :background-state="`url(${smallCardState.low})`" />
-                        <SmallCardComponent :background-state="`url(${smallCardState.low})`" />
-                        <SmallCardComponent :background-state="`url(${smallCardState.low})`" />
-                        <SmallCardComponent :background-state="`url(${smallCardState.low})`" />
+                        <SmallCardComponent :background-state="`url(${smallCardState.low})`" :line-state="lineState.low" :line-no="1"/>
+                        <SmallCardComponent :background-state="`url(${smallCardState.low})`" :line-state="lineState.low" :line-no="2"/>
+                        <SmallCardComponent :background-state="`url(${smallCardState.low})`" :line-state="lineState.low" :line-no="3"/>
+                        <SmallCardComponent :background-state="`url(${smallCardState.low})`" :line-state="lineState.low" :line-no="4"/>
+                        <SmallCardComponent :background-state="`url(${smallCardState.low})`" :line-state="lineState.low" :line-no="5"/>
                         <div class="pt-2 pb-2">
-                            <SmallCardComponent :addition-size="true" :background-state="`url(${smallCardState.low})`" />
+                            <SmallCardComponent :addition-size="true" :background-state="`url(${smallCardState.low})`" :line-state="lineState.low" :line-no="6"/>
                         </div>
-                        <div class="pt-16">
-                            <SmallCardComponent :background-state="`url(${smallCardState.low})`" />
+                        <div class="pt-[5rem]">
+                            <SmallCardComponent :background-state="`url(${smallCardState.low})`" :line-state="lineState.low" :line-no="7"/>
                         </div>
                     </div>
                 </div>
                 <div class="grid basis-1/2 gap-4 pl-1">
-                    <CardComponent class="bg-cover w-[20.4rem] h-64" :background-state="`url(${cardState.low})`" />
-                    <CardComponent class="bg-cover w-[20.4rem] h-64" :background-state="`url(${cardState.low})`" />
-                    <CardComponent class="bg-cover w-[20.4rem] h-64" :background-state="`url(${cardState.low})`" />
+                    <div v-for="item in sampleDataHighestScore">
+                        <CardComponent class="bg-cover w-[20.4rem] h-64" :background-state="`url(${cardState.low})`"
+                            :name="item.risk_disease" :score="item.disease_score" :intro="item.intro" />
+                    </div>
                 </div>
             </div>
         </div>
@@ -33,7 +34,11 @@
 <script setup lang="ts">
 import CardComponent from './cards/CardComponent.vue';
 import SmallCardComponent from './cards/SmallCardComponent.vue';
-const props = defineProps({ background: String });
+const props = defineProps({
+    background: String,
+    sampleDataHighestScore: { type: Array<DcvHealth>, required: true }
+});
+// console.log(props.sampleDataHighestScore)
 const cardState = {
     high: '/src/assets/img/dcv-report/large-box/large-box-high.png',
     med: '/src/assets/img/dcv-report/large-box/large-box-med.png',
@@ -44,5 +49,10 @@ const smallCardState = {
     high: '/src/assets/img/dcv-report/small-box/small-box-high.png',
     med: '/src/assets/img/dcv-report/small-box/small-box-med.png',
     low: '/src/assets/img/dcv-report/small-box/small-box-low.png'
+}
+const lineState = {
+    high: '/src/assets/img/dcv-report/line/lineX_high.png',
+    med: '/src/assets/img/dcv-report/line/lineX_med.png',
+    low: '/src/assets/img/dcv-report/line/lineX_low.png'
 }
 </script>
