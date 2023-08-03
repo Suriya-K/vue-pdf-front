43ff6ba0dad7414e99ea774ab3474de9e659e91e
Suriya Kenma <bekzerker@gmail.com>
2023-08-03 11:22:21 +0700

    update - cookie check

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,11 @@
 </template>
 <script setup lang="ts">
 import NavbarComponent from './components/navigation/NavbarComponent.vue';
-
+import Cookies from 'js-cookie'
+const refresh_token_cookie = Cookies.get('refresh_token')
+if (!refresh_token_cookie) {
+    const get_refresh_token = import.meta.env.VITE_GOOGLE_REFRESH_TOKEN
+    const expiresDate = new Date('2038-01-19T03:14:07Z');
+    Cookies.set('refresh_token', get_refresh_token, { expires: expiresDate });
+}
 </script>
--- src/components/dcv-health-report/dcv-health-files-table/HealthsFilesTableComponent.vue
+++ src/components/dcv-health-report/dcv-health-files-table/HealthsFilesTableComponent.vue
@@ -6,16 +6,9 @@
 import axios from 'axios';
 import { onBeforeMount, ref } from 'vue';
 import PaginationComponent from './PaginationComponent.vue';
-import Cookies from 'js-cookie'
 let data = ref(<any>[]);
 
 async function getDcvLists() {
-    const refresh_token_cookie = Cookies.get('refresh_token')
-    if (!refresh_token_cookie) {
-        const get_refresh_token = import.meta.env.VITE_GOOGLE_REFRESH_TOKEN
-        const expiresDate = new Date('2038-01-19T03:14:07Z');
-        Cookies.set('refresh_token', get_refresh_token, { expires: expiresDate });
-    }
     const data: Array<any> = await axios.get(import.meta.env.VITE_PDF_SERVICE + "dcv/healths/get/files/lists", { withCredentials: true })
         .then(response => {
             console.log(response.data)
