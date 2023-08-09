52f0cc4f0605d83dafcf9511ed50ff2fd1d46974
Suriya Kenma <bekzerker@gmail.com>
2023-08-09 16:43:32 +0700

    add - loading spiner

--- src/components/dcv-health-report/dcv-health-files-table/HealthsFilesTableComponent.vue
+++ src/components/dcv-health-report/dcv-health-files-table/HealthsFilesTableComponent.vue
@@ -1,4 +1,12 @@
 <template>
+    <div class="fixed top-0 left-0 right-0 bottom-0 w-full h-full z-50 overflow-hidden bg-gray-700 opacity-50"
+        v-if="loading">
+        <div class="flex flex-col items-center justify-center pt-64">
+            <div
+                class="inline-block h-24 w-24 animate-spin rounded-full border-8 border-solid border-r-transparent motion-reduce:animate-[spin_1.5s_linear_infinite]">
+            </div>
+        </div>
+    </div>
     <PaginationComponent :items=data :items-per-page=7></PaginationComponent>
 </template>
 
@@ -7,6 +15,7 @@ import axios from 'axios';
 import { onBeforeMount, ref } from 'vue';
 import PaginationComponent from './PaginationComponent.vue';
 let data = ref(<any>[]);
+let loading = ref(true)
 
 async function getDcvLists() {
     const data: Array<any> = await axios.get(import.meta.env.VITE_PDF_SERVICE + "dcv/healths/get/files/lists", { withCredentials: true })
@@ -19,6 +28,7 @@ async function getDcvLists() {
 onBeforeMount(async () => {
     let dcvLists = await getDcvLists();
     let dcvArray: any = [];
+    if (dcvLists) loading.value = false
     for (let i = 0; i < dcvLists.length; i++) {
         dcvArray.push(dcvLists[i])
     }
--- src/components/dcv-health-report/dcv-health-table/HealthsTableComponent.vue
+++ src/components/dcv-health-report/dcv-health-table/HealthsTableComponent.vue
@@ -1,4 +1,12 @@
 <template>
+    <div class="fixed top-0 left-0 right-0 bottom-0 w-full h-full z-50 overflow-hidden bg-gray-700 opacity-50"
+        v-if="loading">
+        <div class="flex flex-col items-center justify-center pt-64">
+            <div
+                class="inline-block h-24 w-24 animate-spin rounded-full border-8 border-solid border-r-transparent motion-reduce:animate-[spin_1.5s_linear_infinite]">
+            </div>
+        </div>
+    </div>
     <PaginationComponent :items=data :items-per-page=7 :id="props.id"></PaginationComponent>
 </template>
 
@@ -8,6 +16,7 @@ import { onMounted, ref } from 'vue';
 import PaginationComponent from './PaginationComponent.vue';
 
 let data = ref(<any>[]);
+let loading = ref(true);
 const props = defineProps({
     id: { type: String, required: true }
 })
@@ -23,6 +32,7 @@ async function getDcvLists() {
 onMounted(async () => {
     let dcvLists = await getDcvLists();
     let dcvArray: any = [];
+    if (dcvLists) loading.value = false
     for (let key in dcvLists) {
         dcvArray.push(key);
     }
